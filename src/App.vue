c842413a15f20b403251f22aeabd11181eda1b46
guziyi <a1725137488@gmail.com>
2024-03-15 17:26:57 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+  <div class="bg-red-500">
+    sokoban {{  map }} 
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useMapStore} from './store/map'
+
+const { map} = useMapStore()
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/store/tests/map.spec.ts
@@ -0,0 +1,8 @@
+import { it, expect, describe } from 'vitest'
+
+
+describe('map', () => {
+  it('should', () => {
+    expect(true).toBe(true)
+  })
+})
