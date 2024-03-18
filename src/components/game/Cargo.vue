7916214482b93242fa1f4f9bbfecd95b59c6574f
guziyi <a1725137488@gmail.com>
2024-03-18 17:56:16 +0800

    feat:add Cargo

---
+++ src/components/game/Cargo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="absolute" :style="position">
+    <img :src="cargoImg" alt="player">
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import cargoImg from '@/assets/cargo.png'
+
+const cargo = {
+  x: 2,
+  y: 2
+}
+
+const { position } = usePosition()
+function usePosition() {
+  // const { player } = usePlayerStore()
+  const STEP = 32
+  const position = computed(() => {
+    return {
+      left: cargo.x * STEP + 'px',
+      top: cargo.y * STEP + 'px'
+    }
+  })
+  return {
+    position
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/game/Game.vue
+++ src/components/game/Game.vue
@@ -2,12 +2,14 @@
   <div class="game-box">
     <Map />
     <Player />
+    <Cargo />
   </div>
 </template>
 
 <script setup lang="ts">
 import Map from '@/components/game/Map.vue'
 import Player from '@/components/game/Player.vue'
+import Cargo from '@/components/game/Cargo.vue'
 
 </script>
 
